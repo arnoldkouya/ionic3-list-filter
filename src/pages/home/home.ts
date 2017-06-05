bb6424ca399fed182fdacc1a488e0c2b85281393
Teerasej <teerasej@gmail.com>
2017-06-05 23:51:09 +0700

    Add keyword and binded to list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  keyword;
+  provinces = [];
+  sourceProvinces = [
+    'กรุงเทพมหานคร'
+    , 'เชียงใหม่'
+    , 'ขอนแก่น'
+    , 'อุบลราชธานี'
+    , 'ภูเก็ต'
+    , 'กระบี่'
+  ];
 
+  constructor(public navCtrl: NavController) {
+    this.provinces = this.sourceProvinces;
   }
+
+  setFilter() {
+    let keyword = this.keyword;
+
+    this.provinces = this.sourceProvinces;
+    
+    this.provinces = this.provinces.filter((province) => {
+      return province.indexOf(keyword) > -1;
+    })
+    
+  }
+
 
 }
